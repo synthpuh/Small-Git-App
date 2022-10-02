90d92303e2cc38b6b1980ad77b6e9d6af7b78464
synthpuh <amaeth.olga@gmail.com>
2022-10-02 13:57:06 +0300

    Initial Commit

---
+++ Small Git App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Small Git App
+//
+//  Created by Ольга Шубина on 02.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
